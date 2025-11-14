02bfbc270c406845ddce145f673627d64268c05c
asyakandyba <asyakan03@gmail.com>
2025-11-14 13:52:34 +0200

    Add share on fb & Clean up

--- js/controllers/meme.controller.js
+++ js/controllers/meme.controller.js
@@ -80,11 +80,6 @@ function onAlignLine(elBtn) {
     renderMeme()
 }
 
-function onSetFunc(func) {
-    func()
-    renderMeme()
-}
-
 function onSetTxt(elTxtInput) {
     setTxt(elTxtInput.value)
     renderMeme()
@@ -96,7 +91,8 @@ function onSetColor(elClrInput) {
 }
 
 function onSetSize(elSizeInput) {
-    setTxtSize(+elSizeInput.value)
+    const diff = elSizeInput.value === 'bigger' ? 5 : -5
+    setTxtSize(diff)
     renderMeme()
 }
 
@@ -109,5 +105,42 @@ function onDownloadMeme(elLink) {
     const dataUrl = gElCanvas.toDataURL()
 
     elLink.href = dataUrl
-    elLink.download = 'my-canvas'
+    elLink.download = 'my-meme'
+}
+
+function onShareImg(ev) {
+    ev.preventDefault()
+    const canvasData = gElCanvas.toDataURL('image/jpeg')
+
+    // After a succesful upload, allow the user to share on Facebook
+    function onSuccess(uploadedImgUrl) {
+        const encodedUploadedImgUrl = encodeURIComponent(uploadedImgUrl)
+        console.log('encodedUploadedImgUrl:', encodedUploadedImgUrl)
+        window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodedUploadedImgUrl}&t=${encodedUploadedImgUrl}`)
+
+    }
+    uploadImg(canvasData, onSuccess)
+}
+
+
+// on submit call to this function
+
+async function uploadImg(imgData, onSuccess) {
+    const CLOUD_NAME = 'webify'
+    const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
+    const formData = new FormData()
+    formData.append('file', imgData)
+    formData.append('upload_preset', 'webify')
+    try {
+        const res = await fetch(UPLOAD_URL, {
+            method: 'POST',
+            body: formData
+        })
+        const data = await res.json()
+        console.log('Cloudinary response:', data)
+        onSuccess(data.secure_url)
+
+    } catch (err) {
+        console.log(err)
+    }
 }
